e66868de4a7fa23a5e78d303619852c9e933a091
Jacques KAISER <jackokaiser@gmail.com>
2014-10-21 21:49:29 +0200

    different material for all monsters

--- app/js/monster.js
+++ app/js/monster.js
@@ -1,10 +1,11 @@
 (function() {
   var monsterGeometry = new THREE.BoxGeometry( 2, 2, 2 );
-  var monsterMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
   var popDistance = 200;
   var monsterSpeed = 10;
 
   var Monster = function() {
+    var monsterMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+
     this.mesh = new THREE.Mesh( monsterGeometry, monsterMaterial );
     var randomAngle = Math.random() * Math.PI * 2;
     var xCoord = Math.cos(randomAngle)*popDistance;
@@ -15,7 +16,7 @@
     window.scene.add(this.mesh);
   };
   Monster.prototype.isTouchingPlayer = function() {
-    var epsilon = 0.001;
+    var epsilon = 0.1;
     return (this.mesh.position.lengthSq() < epsilon);
   };
   Monster.prototype.constructor = Monster;
@@ -23,6 +24,10 @@
     this.mesh.material.color = new THREE.Color(0xff0000);
   };
   Monster.prototype.update = function(dt) {
+    if (this.isTouchingPlayer()) {
+      return true;
+    }
+
     var playerToMonster = new THREE.Vector3()
       .copy(this.mesh.position)
       .normalize();
